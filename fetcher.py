5b6a0503661c685522b80dbe860ff1205de0d960
Markus Zy <zyxar@outlook.com>
2013-02-23 22:52:23 +0800

    add cache in fetcher.py

--- fetcher.py
+++ fetcher.py
@@ -28,6 +28,7 @@ class Fetcher():
     block_sz = 32 * 1024
     def __init__(self, url=None):
         self.url = url
+        self.cache = {}
 
     def fetch(self, url=None):
         payload = url or self.url
@@ -38,30 +39,39 @@ class Fetcher():
             self.retrieve(uu, filename)
             uu.close()
 
-    def retrieve(self, socket, filename):
+    def retrieve(self, socket, filename, cache_enabled=False):
         size = int(socket.info().getheader('content-length'))
+        url = socket.geturl()
         if exists(filename) and getsize(filename) == size:
             try:
                 print "File %s already exists." % filename
             except UnicodeEncodeError:
                 print "File %s already exists." % filename.encode('UTF-8')
+            if cache_enabled and not self.cache.has_key(url):
+                ff = open(filename, 'rb')
+                self.cache[url] = ff.read()
+                ff.close()
         else:
             try:
                 print "Downloading: %s. Bytes: %d" % (filename, size)
             except UnicodeEncodeError:
                 print "Downloading: %s. Bytes: %d" % (filename.encode('UTF-8'), size)
-            ss = StringIO()
-            dld = 0
-            while True:
-                buf = socket.read(self.block_sz)
-                if not buf:
-                    break
-                dld += len(buf)
-                ss.write(buf)
-                status(dld*1./size)
+            if cache_enabled and self.cache.has_key(url):
+                pass
+            else:
+                ss = StringIO()
+                dld = 0
+                while True:
+                    buf = socket.read(self.block_sz)
+                    if not buf:
+                        break
+                    dld += len(buf)
+                    ss.write(buf)
+                    status(dld*1./size)
+                self.cache[url] = ss.getvalue()
+                ss.close()
             print
-            savefile(filename, ss.getvalue())
-            ss.close()
+            savefile(filename, self.cache[url])
 
 if __name__ == '__main__':
     fetcher = Fetcher()
